a9d1128008adc9df53721439eb4b8dd695e20ed3
rcremese <rcremese@DESKTOP-AR0N0MC.localdomain>
2023-01-31 18:46:18 +0100

    Implementation d'une simulation d'équation de diffusion dans un milieu contraint

--- src/snake_ai/physim/diffusion_solver.py
+++ src/snake_ai/physim/diffusion_solver.py
@@ -1,24 +1,32 @@
 from phi.jax import flow
+from phi.physics.fluid import apply_boundary_conditions, Obstacle
 import matplotlib.pyplot as plt
 
-
 bounds = flow.Box(x=10, y=10)
-obstacle = flow.Obstacle(flow.Box(x=(5, 6), y=(5, 6))) 
+obstacle = Obstacle(flow.Box(x=(0, 4), y=(0, 4)))  
 
-concentration = 100 * flow.CenteredGrid(flow.Sphere(center=flow.vec(x=2, y=2), radius=2), bounds=bounds, resolution=100)
+initial_distrib = flow.Sphere(x=5, y=5, radius=2)
+concentration = 100 * flow.CenteredGrid(initial_distrib, bounds=bounds, resolution=100)
 
 D = 1
 dt = 0.5 * concentration.dx ** 2 / D 
 
 @flow.math.jit_compile
-def step(concentration, obstacle : flow.Obstacle):
+def step(concentration, obstacle_field : flow.GeometryMask):
     temp =  flow.diffuse.explicit(concentration, diffusivity=D, dt=dt, substeps=10)
-    return obstacle @ temp
+    return obstacle_field * temp
 
-trajectory = [concentration]
-for i in range(200):
-    concentration = step(concentration, obstacle)
-    if i % 20 == 0:
+@flow.math.jit_compile
+def new_step(velocity, obstacle):
+    temp =  flow.diffuse.explicit(velocity, diffusivity=D, dt=dt, substeps=10)
+    return apply_boundary_conditions(temp, obstacle)    
+T_max = 200
+i_print = T_max // 5
+
+trajectory = [apply_boundary_conditions(concentration, [obstacle])]
+for i in range(T_max):
+    concentration = new_step(concentration, [obstacle])
+    if i % i_print == 0:
         trajectory.append(concentration)
 trajectory = flow.field.stack(trajectory, flow.batch('time'))
 fig = flow.vis.plot(trajectory, show_color_bar=False)
